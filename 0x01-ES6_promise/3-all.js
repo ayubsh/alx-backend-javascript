24250c9ea2db75c129ea297a3f8c76fc88aaacc1
ayubsh <ibrahimayubsheikh@gmail.com>
2023-09-27 09:31:14 +0300

    promise.all

---
+++ 0x01-ES6_promise/3-all.js
@@ -0,0 +1,9 @@
+import { createUser, uploadPhoto } from './utils';
+
+export default function handleProfileSignup() {
+  return Promise.all([createUser(), uploadPhoto()])
+    .then((res) => {
+      console.log(`${res[1].body} ${res[0].firstName} ${res[0].lastName}`);
+    })
+    .catch(() => console.log('Signup system offline'));
+}
