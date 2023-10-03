457579ff06a606737f93f0d9652634ce6af0bb06
ayubsh <ibrahimayubsheikh@gmail.com>
2023-10-03 21:22:40 +0300

    map of objs

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const gr = new Map();
+
+  gr.set('Apples', 10);
+  gr.set('Tomatoes', 10);
+  gr.set('Pasta', 1);
+  gr.set('Rice', 1);
+  gr.set('Banana', 5);
+  return gr;
+}
