52336efae030f4fb9c37050d2a9d17af484dadf7
ayubsh <ibrahimayubsheikh@gmail.com>
2023-10-04 19:14:17 +0300

    functions specific to employees

--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -45,3 +45,16 @@ export function createEmployee(salary: number | string) {
     return new Director();
   }
 }
+
+
+function isDirector(employee: Director | Teacher): employee is Director {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: Director | Teacher) {
+  if (isDirector(employee)) {
+    return employee.workDirectorTasks();
+  } else {
+    return employee.workTeacherTasks();
+  }
+}
