099cb755b057d173937b846ac48758b1be44b10f
ayubsh <ibrahimayubsheikh@gmail.com>
2023-11-23 10:47:56 +0300

    unit testing

--- 0x06-unittests_in_js/0-calcul.test.js
+++ 0x06-unittests_in_js/0-calcul.test.js
@@ -3,27 +3,22 @@ const calculateNumber = require('./0-calcul');
 
 describe('calculateNumber', () => {
   it('round to whole number', () => {
-    expect.assertions(1);
     assert.strictEqual(calculateNumber(3.0, 2.0), 5);
   });
 
   it('whole numbers', () => {
-    expect.assertions(1);
     assert.strictEqual(calculateNumber(4, 5), 9);
   });
 
   it('floating points', () => {
-    expect.assertions(1);
     assert.strictEqual(calculateNumber(0.5, 0.5), 2);
   });
 
   it('round down', () => {
-    expect.assertions(1);
     assert.strictEqual(calculateNumber(1.2, 3.3), 4);
   });
 
   it('round up', () => {
-    expect.assertions(1);
     assert.strictEqual(calculateNumber(5.5, 6.6), 13);
   });
 });
